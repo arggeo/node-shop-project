a9143fdb3bd01c91fd08985686970ea9a7c5a8f5
argygeor <signup@arggeo.com>
2021-02-12 18:07:14 +0200

    Added password reset functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const User = require('../models/user');
 
 // Encryption
 const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
 
 // Mailer
 const nodemailer = require('nodemailer');
@@ -77,7 +78,7 @@ exports.getSignup = (req, res, next) => {
       path: '/signup',
       pageTitle: 'Signup',
       errorMessage: message
-   })
+   });
 }
 
 exports.postSignup = (req, res, next) => {
@@ -85,7 +86,7 @@ exports.postSignup = (req, res, next) => {
    const password = req.body.password;
    const confirmPassword = req.body.confirmPassword;
 
-   User.findOne({ email: email })
+   User.findOne({email: email})
       .then(userDoc => {
          if (userDoc) {
             req.flash('error', 'E-mail already exists.');
@@ -112,6 +113,103 @@ exports.postSignup = (req, res, next) => {
                });
             })
             .catch(err => console.log(err));
+      })
+      .catch(err => console.log(err));
+}
+
+exports.getReset = (req, res, next) => {
+   let message = req.flash('error');
+   if (message.length > 0) {
+      message = message[0];
+   } else {
+      message = null;
+   }
+   res.render('auth/reset', {
+      path: '/reset',
+      pageTitle: 'Reset Password',
+      errorMessage: message
+   });
+}
+
+exports.postReset = (req, res, next) => {
+   crypto.randomBytes(32, (err, buffer) => {
+      if (err) {
+         console.log(err);
+         return res.status(302).redirect('/reset');
+      }
+      const token = buffer.toString('hex');
+      User.findOne({ email: req.body.email })
+         .then(user => {
+            if (!user) {
+               req.flash('error', 'No account with that email found.');
+               return res.status(302).redirect('/reset');
+            }
+            user.resetToken = token;
+            user.resetTokenExpiration = Date.now() + 3600000;
+            return user.save();
+         })
+         .then(result => {
+            res.status(302).redirect('/');
+            transporter.sendMail({
+               to: req.body.email,
+               from: 'xxxxxxxxx@gmail.com',
+               subject: 'Password reset',
+               html: `
+                  <p>You requested a password reset.</p>
+                  <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>
+               `
+            });
+         })
+         .catch(err => console.log(err));
+   });
+}
+
+exports.getNewPassword = (req, res, next) => {
+   const token = req.params.token;
+   User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } })
+      .then(user => {
+         let message = req.flash('error');
+         if (message.length > 0) {
+            message = message[0];
+         } else {
+            message = null;
+         }
+         res.render('auth/new-password', {
+            path: '/new-password',
+            pageTitle: 'New Password',
+            errorMessage: message,
+            userId: user._id.toString(),
+            passwordToken: token
+         });
+      })
+      .catch(err => console.log(err));
+}
+
+exports.postNewPassword = (req, res, next) => {
+   const newPassword = req.body.password;
+   const userId = req.body.userId;
+   const passwordToken = req.body.passwordToken;
+   let resetUser;
+
+   User.findOne({
+      resetToken: passwordToken,
+      resetTokenExpiration: {
+         $gt: Date.now()
+      },
+      _id: userId
+   })
+      .then(user => {
+         resetUser = user;
+         return bcrypt.hash(newPassword, 12);
+      })
+      .then(hashedPassword => {
+         resetUser.password = hashedPassword;
+         resetUser.resetToken = null;
+         resetUser.resetTokenExpiration = undefined;
+         return resetUser.save();
+      })
+      .then(result => {
+         res.status(302).redirect('/login');
       })
       .catch(err => console.log(err));
 }
--- routes/auth.js
+++ routes/auth.js
@@ -7,10 +7,14 @@ const authController = require('../controllers/auth');
 // GET => /login
 router.get('/login', authController.getLogin);
 router.get('/signup', authController.getSignup);
+router.get('/reset', authController.getReset);
+router.get('/reset/:token', authController.getNewPassword);
 
 // POST => /login
 router.post('/login', authController.postLogin);
 router.post('/logout', authController.postLogout);
 router.post('/signup', authController.postSignup);
+router.post('/reset', authController.postReset);
+router.post('/new-password', authController.postNewPassword);
 
 module.exports = router;
