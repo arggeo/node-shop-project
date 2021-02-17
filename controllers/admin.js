14fd3e7e45f005d5908cb6583957bb3d6dc955a6
argygeor <signup@arggeo.com>
2021-02-18 00:26:00 +0200

    Refactored the delete function to async

--- controllers/admin.js
+++ controllers/admin.js
@@ -177,8 +177,8 @@ exports.getProducts = (req, res, next) => {
       });
 }
 
-exports.postDeleteProduct = (req, res, next) => {
-   const prodID = req.body.productID;
+exports.deleteProduct = (req, res, next) => {
+   const prodID = req.params.productId;
    Product.findById(prodID)
       .then(product => {
         if (!product) {
@@ -188,11 +188,9 @@ exports.postDeleteProduct = (req, res, next) => {
         return Product.deleteOne({ _id: prodID, userId: req.user._id });
       })
       .then(() => {
-         res.redirect('/admin/products');
+         res.status(200).json({ message: 'Success!' });
       })
       .catch(err => {
-         const error = new Error(err);
-         error.httpStatusCode = 500;
-         return next(error);
+         res.status(500).json({ message: 'Deleting product failed.' });
       });
 }
