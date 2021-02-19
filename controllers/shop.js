d7897b3faa6769e3aa325722c8f6c6d1add37175
argygeor <signup@arggeo.com>
2021-02-19 12:21:24 +0200

    Added stripe payment gateway

--- controllers/shop.js
+++ controllers/shop.js
@@ -9,6 +9,9 @@ const path = require('path');
 const fs = require('fs');
 const PDFDocument = require('pdfkit');
 
+// Stripe
+const stripe = require('stripe')('xxxxxxxxx');
+
 const ITEMS_PER_PAGE = 2;
 
 exports.getProducts = (req, res, next) => {
@@ -135,7 +138,50 @@ exports.postCartDeleteProduct = (req, res, next) => {
       });
 }
 
-exports.postOrder = (req, res, next) => {
+exports.getCheckout = (req, res, next) => {
+   let products;
+   let total = 0;
+   req.user.populate('cart.items.productId')
+      .execPopulate()
+      .then(user => {
+         products = user.cart.items;
+         total = 0;
+         products.forEach(p => {
+            total += p.quantity * p.productId.price;
+         });
+
+         return stripe.checkout.sessions.create({
+            payment_intent_data: ['card'],
+            line_items: products.map(p => {
+              return {
+                 name: p.productId.title,
+                 description: p.productId.description,
+                 amount: p.productId.price * 100,
+                 currency: 'usd',
+                 quantity: p.quantity
+              }
+            }),
+            success_url: req.protocol + '://' + req.get('host') + '/checkout/success',
+            cancel_url: req.protocol + '://' + req.get('host') + '/checkout/cancel',
+         });
+      })
+      .then(session => {
+         res.render('shop/checkout', {
+            pageTitle: 'Checkout',
+            path: '/checkout',
+            products: products,
+            totalSum: total,
+            sessionId: session.id
+         });
+      })
+      .catch(err => {
+         const error = new Error(err);
+         error.httpStatusCode = 500;
+         return next(error);
+      });
+}
+
+exports.getCheckoutSuccess = (req, res, next) => {
    req.user.populate('cart.items.productId')
       .execPopulate()
       .then(user => {
